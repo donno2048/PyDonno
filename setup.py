0f1f70160471ae6234201e84e8b9ea224306b56e
Elisha Hollander <just4now666666@gmail.com>
2021-03-20 21:53:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ class Install(install):
             if len(line) > 1: call(['pip', 'install', line.replace('\n', '')])
 setup(
     name = 'PyDonno',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'All my packages',
     long_description = open('README.md').read(),
+    long_description_content_type="text/markdown",
     url = 'https://github.com/donno2048/PyDonno',
     license = 'MIT',
     author = 'Elisha Hollander',
