309ab41c9e61d1560ad8bd0ee96a147966204dae
donno2048 <just4now666666@gmail.com>
2026-04-26 21:06:22 +0300

    fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 from setuptools.command.install import install
 from subprocess import call
+from sys import executable
 class Install(install):
     def run(self):
         install.run(self)
         for line in open('requirements.txt').readlines():
-            if len(line) > 1: call(['pip3', 'install', line.replace('\n', '')])
+            if len(line) > 1 and not line.startswith("#"): call([executable, '-m', 'pip', 'install', line.replace('\n', '')])
 setup(
     name = 'PyDonno',
-    version = '1.3.0',
+    version = '1.3.1',
     description = 'All my packages',
     long_description = open('README.md').read().replace('`', '').replace('sh', '').replace('\n', '\n\n'),
     long_description_content_type="text/markdown",
